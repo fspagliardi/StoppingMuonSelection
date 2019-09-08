655e26dc99da8f9237e0f45035e8710af3c1ae9a
fspagliardi <fabio.spagliardi@physics.ox.ac.uk>
2019-09-08 17:28:50 -0500

    Created

---
+++ TruedEdxHelper.cxx
@@ -0,0 +1,22 @@
+/***
+  Class containing useful functions for geometry.
+
+*/
+#ifndef TRUEDEDX_HELPER_CXX
+#define TRUEDEDX_HELPER_CXX
+
+#include "TruedEdxHelper.h"
+
+namespace stoppingcosmicmuonselection {
+
+  TruedEdxHelper::TruedEdxHelper() {
+
+  }
+
+  TruedEdxHelper::~TruedEdxHelper() {
+
+  }
+
+} // end of namespace stoppingcosmicmuonselection
+
+#endif
---
+++ TruedEdxHelper.h
@@ -0,0 +1,28 @@
+/***
+  Class containing useful functions for geometry.
+
+*/
+#ifndef TRUEDEDX_HELPER_H
+#define TRUEDEDX_HELPER_H
+
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/TPCGeo.h"
+#include "larcorealg/Geometry/GeometryCore.h"
+#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
+#include "TVector3.h"
+#include "TMath.h"
+
+namespace stoppingcosmicmuonselection {
+
+  class TruedEdxHelper {
+
+  public:
+    TruedEdxHelper();
+    ~TruedEdxHelper();
+
+
+  private:
+  };
+}
+
+#endif
