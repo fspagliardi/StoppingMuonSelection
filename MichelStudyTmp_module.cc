0a8d683adb90e4f4b88f908d8cf4992381d050d8
fspagliardi <fabio.spagliardi@physics.ox.ac.uk>
2020-01-13 10:43:43 -0600

    Add support for new space point alorithm class

--- MichelStudyTmp_module.cc
+++ MichelStudyTmp_module.cc
@@ -67,8 +67,7 @@ namespace stoppingcosmicmuonselection {
       // Check if the track is missing some space points (need to get
       // an handle on the track)
       const recob::Track &track = selectorAlg.GetTrackFromPFParticle(evt,thisParticle);
-      spAlg.SetT0(selectorAlg.GetTrackProperties().trackT0);
-      if(!spAlg.IsGoodTrack(track,spacePoints,fIsRecoSelectedAnodeCrosser)) {
+      if(!spAlg.IsGoodTrack(track,spacePoints,selectorAlg.GetTrackProperties())) {
         std::cout << "Space point alg: " << "TrackID: " << track.ID() << " not accepted." << std::endl;
         continue;
       }
