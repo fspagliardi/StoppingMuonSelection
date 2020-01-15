c388e45852a52a91ea52b4a6c8437bde00efa88f
fspagliardi <fabio.spagliardi@physics.ox.ac.uk>
2020-01-15 11:27:01 -0600

    Un-comment selection based on space points

--- MichelStudyTmp_module.cc
+++ MichelStudyTmp_module.cc
@@ -66,11 +66,11 @@ namespace stoppingcosmicmuonselection {
 
       // Check if the track is missing some space points (need to get
       // an handle on the track)
-      //const recob::Track &track = selectorAlg.GetTrackFromPFParticle(evt,thisParticle);
-      //if(!spAlg.IsGoodTrack(track,spacePoints,selectorAlg.GetTrackProperties())) {
-        //std::cout << "Space point alg: " << "TrackID: " << track.ID() << " not accepted." << std::endl;
-        //continue;
-      //}
+      const recob::Track &track = selectorAlg.GetTrackFromPFParticle(evt,thisParticle);
+      if(!spAlg.IsGoodTrack(track,spacePoints,selectorAlg.GetTrackProperties())) {
+        std::cout << "Space point alg: " << "TrackID: " << track.ID() << " not accepted." << std::endl;
+        continue;
+      }
 
       // Check if the matched PFParticle is a true stopping muon
       if (fIsRecoSelectedCathodeCrosser)
